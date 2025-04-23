78fc939e8aaa6449478fed2c06804842d84e9001
amina antonova <aminantonova@gmail.com>
2025-04-24 00:08:26 +0300

    pomadd

---
+++ Project_Web/src/main/java/com/example/selenium/SeleniumTest.java
@@ -0,0 +1,23 @@
+package com.example.selenium;
+
+import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class SeleniumTest {
+    public static void main(String[] args) throws Exception {
+        // Gson
+        String gsonJson = new Gson().toJson("Hello from Gson!");
+        System.out.println("Gson JSON: " + gsonJson);
+
+        // Jackson
+        String json = "{\"name\":\"Ivan\",\"age\":35}";
+        ObjectMapper mapper = new ObjectMapper();
+        User user = mapper.readValue(json, User.class);
+        System.out.println("Jackson name: " + user.name);
+    }
+
+    public static class User {
+        public String name;
+        public int age;
+    }
+}
